aa47513a7f8d7f863f0e6ff5cc71d73b5f5b5500
RockLeeHere <74140281+RockLeeHere@users.noreply.github.com>
2021-09-03 12:43:32 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,30 @@
+<!DOCTYPE html>
+<html>
 
+<head>
+	<title>
+		How to set background-image for
+		the body element using CSS ?
+	</title>
+
+	<style>
+		body {
+			background-image: url(
+"https://media.geeksforgeeks.org/wp-content/cdn-uploads/CSS.png");
+			text-align: center;
+		}
+	</style>
+</head>
+
+<body>
+	<h1 style="color: white;">
+		GeeksforGeeks
+	</h1>
+
+	<h3>
+		Set background-image for
+		the body element
+	</h3>
+</body>
+
+</html>
